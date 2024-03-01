443eb30eccfc4ba6b83ca92128413abd4a4b02bb
Julia Lenz <julia.lenzz03@gmail.com>
2024-03-01 13:20:54 -0600

    added portfolio pattern

---
+++ wordpress/wp-content/plugins/no-pulp-table/patterns/portfolio.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Title: Portfolio Page
+ * Slug: high-pulp-blocks/portfolio
+ * Categories: featured, pulpy
+ *
+ * More headers can be found at:
+ * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+ */
+?>
+<!-- wp:heading {"style":{"color":{"background":"#c8dafd"},"typography":{"textTransform":"capitalize"}},"borderStyle":"none","borderColor":"#c8dafd"} -->
+<h2 class="wp-block-heading has-background" style="background-color:#c8dafd;text-transform:capitalize">WordPress Development Author Website: <strong>Victoria Schwab</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>As the final of my WordPress Development Class, I created a dynamic WordPress website dedicated to showcasing the works of one of my favorite authors. This project involved the development of a child theme alongside the implementation of plugins and blocks to enhance functionality and user engagement. Notably, I built a custom plugin designed to display author's books and accompanying reviews, thus enriching the visitor's experience. The project's database infrastructure is managed and optimized utilizing phpMyAdmin, ensuring seamless data management and accessibility.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Skills</h4>
+<!-- /wp:heading -->
+
+<!-- wp:blockart/icon-list {"clientId":"de73d3c3"} -->
+<div class="blockart-icon-list blockart-icon-list-de73d3c3 blockart-vertical blockart-default"><!-- wp:blockart/icon-list-item {"clientId":"9abfebd5"} -->
+	<div class="blockart-icon-list-item blockart-icon-list-item-9abfebd5"><span class="blockart-icon-wrapper"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M7.7 16.8l-4.3-4.1L2 14.1l5.7 5.4L21.9 5.9l-1.5-1.4L7.7 16.8z"></path></svg></span></div>
+	<!-- /wp:blockart/icon-list-item -->
+
+	<!-- wp:blockart/icon-list-item {"clientId":"e6a08a94"} -->
+	<div class="blockart-icon-list-item blockart-icon-list-item-e6a08a94"><span class="blockart-icon-wrapper"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M7.7 16.8l-4.3-4.1L2 14.1l5.7 5.4L21.9 5.9l-1.5-1.4L7.7 16.8z"></path></svg></span></div>
+	<!-- /wp:blockart/icon-list-item -->
+
+	<!-- wp:blockart/icon-list-item {"clientId":"512e2ef7"} -->
+	<div class="blockart-icon-list-item blockart-icon-list-item-512e2ef7"><span class="blockart-icon-wrapper"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="blockart-icon" aria-hidden="true"><path d="M7.7 16.8l-4.3-4.1L2 14.1l5.7 5.4L21.9 5.9l-1.5-1.4L7.7 16.8z"></path></svg></span></div>
+	<!-- /wp:blockart/icon-list-item --></div>
+<!-- /wp:blockart/icon-list -->
+
+<!-- wp:blockart/button {"clientId":"1a375222"} -->
+<div class="blockart-buttons blockart-buttons-1a375222 is-wrap"><!-- wp:blockart/button-inner {"clientId":"2c89c2b4","link":{"url":"","newTab":true},"text":"Live Site URL","size":"small","icon":{"icon":"globe-solid","enable":false},"iconPosition":"left","iconSize":{"desktop":{"value":23,"unit":"px"}},"color1":"#000000","background1":{"background":1,"type":"color","color":"#C8DAFD"},"blockMargin":{"dimension":1,"desktop":{"lock":false,"unit":"%","top":2,"right":2,"bottom":2,"left":0}}} -->
+	<div class="blockart-button blockart-button-2c89c2b4 "><a class="blockart-button-link is-small is-style-filled" target="_blank" rel="noopener">Live Site URL</a></div>
+	<!-- /wp:blockart/button-inner --></div>
+<!-- /wp:blockart/button -->
+
+<!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped">
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large"><img src="https://place-hold.it/150" alt="" /></figure>
+	<!-- /wp:image -->
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large"><img src="https://place-hold.it/150" alt="" /></figure>
+	<!-- /wp:image -->
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large"><img src="https://place-hold.it/150" alt="" /></figure>
+	<!-- /wp:image -->
+
+
+
+<!-- /wp:gallery -->
